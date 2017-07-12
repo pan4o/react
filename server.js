e4052b442c78f15728f4f1d9908621467279229f
Pancho <panchenko923@gmail.com>
2017-07-12 23:40:40 +0300

    выводим index.html через сервер

---
+++ server.js
@@ -0,0 +1,19 @@
+
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    path = require('path');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.listen(3000, function () {
+    console.log('work on 3000 port!');
+});
