469b6711e131615ba0378194785ab05c99e9930f
Pancho <panchenko923@gmail.com>
2017-07-13 01:19:17 +0300

    подключен react

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+	entry: {
+		main: "./src/js/entry-point",
+	},
+	output: {
+		filename: "./src/bundle.js"
+	},
+	watch: true,
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			}
+		]
+	}
+}
