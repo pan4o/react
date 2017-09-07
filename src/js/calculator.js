0520c2c63e61d9c423247723152cc0b6ff87ad97
Pancho <panchenko923@gmail.com>
2017-09-07 23:25:40 +0300

    add some functions

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -7,26 +7,43 @@ var scaleNames = {
 	f: 'Fahrenhei'
 };
 
+function toCelsius(fahrenheit) {
+	return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+	return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert (temperature, convert) {
+	const input = parseFloat(temperature);
+
+	if(Number.isNaN(input)) {
+		return '';
+	}
+
+	const output = convert(input);
+	const rounded = Math.round(output * 1000) / 1000;
+
+	return rounded.toString();
+}
+
 class Calculator extends React.Component {
 
 	constructor (props) {
 
 		super(props);
 
-		this.state = {
-			temperature: ''
-		};
+		this.state = {temperature: ''};
 
 		this.handleChange = this.handleChange.bind(this);
 
 	}
 
-	handleChange (event) {
-
-		var target = event.target;
+	handleChange (e) {
 
 		this.setState({
-			temperature: target.value
+			temperature: e.target.value
 		});
 
 	}
